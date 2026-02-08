1ec8f94af2cb5135d3aa13e248138f5fe38d4490
Gabriel <gabrielmn.machadonunes@gmail.com>
2026-02-08 12:49:31 -0300

    feature-idade-added

--- novo_arquivo.py
+++ novo_arquivo.py
@@ -2,7 +2,14 @@ def get_name():
     name = input("what's your name? ")
     return name
 
+def get_idade():
+    idade = int(input("idade: "))
+    return idade
+
 print("2026")
 
 nome = get_name()
 print(nome)
+
+idade = get_idade()
+print(idade)
